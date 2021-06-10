45a60cb7548c74a07c531f12a539f572236cd820
dvmarinoff <dvmarinoff@gmail.com>
2021-06-10 17:16:34 +0300

    add heart rate data and connection simulation to mock

--- test/mock.js
+++ test/mock.js
@@ -12,6 +12,18 @@ class TrainerMock {
         xf.sub('ui:workoutStart', self.run.bind(self));
         xf.sub('ui:watchPause', self.stop.bind(self));
 
+        self.id = 'ble:controllable';
+        self.name = 'Tacx Flux 46731';
+
+        xf.dispatch(`${self.id}:connected`);
+        xf.dispatch(`${self.id}:name`, self.name);
+
+        self.hrId = 'ble:hrm';
+        self.hrName = 'Tacx HRB 20483';
+
+        xf.dispatch(`${self.hrId}:connected`);
+        xf.dispatch(`${self.hrId}:name`, self.hrName);
+
         console.warn('Trainer Mock Data is ON!');
     }
     run() {
@@ -23,7 +35,7 @@ class TrainerMock {
         clearInterval(self.interval);
     }
     broadcast(handler) {
-        const interval = setInterval(handler, 1000);
+        const interval = setInterval(handler, 500);
         return interval;
     }
     indoorBikeData() {
@@ -31,18 +43,26 @@ class TrainerMock {
         xf.dispatch('power', self.power(self.powerTarget));
         xf.dispatch('speed', self.speed(20));
         xf.dispatch('cadence', self.cadence(80));
+        xf.dispatch('heartRate', self.heartRate(163));
     }
     onPowerTarget(powerTarget) {
         this.powerTarget = powerTarget;
     }
     power(prev) {
-        return prev + rand(-4, 4);
+        let low = rand(1,100);
+        if(low === 90) {
+            return 0;
+        }
+        return prev + rand(-8, 8);
     }
     cadence(prev) {
-        return prev + rand(-3, 3);
+        return prev + rand(0, 1);
     }
     speed(prev) {
         return prev + rand(-0.1, 0.1);
+    }
+    heartRate(prev) {
+        return prev + rand(2, 2);
     }
 }
 
