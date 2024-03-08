eff9e683fe6855f28ba15b29d3f495c01e869b63
dvmarinoff <dvmarinoff@gmail.com>
2024-03-08 14:12:34 +0200

    update switch device types, control methods for reactive connectable

--- src/ble/reactive-connectable.js
+++ src/ble/reactive-connectable.js
@@ -1,28 +1,38 @@
-import { equals, xf, print, } from '../functions.js';
+import { equals, exists, xf, print, } from '../functions.js';
 import { models } from '../models/models.js';
 import Connectable from './connectable.js';
 import { webBle } from './web-ble.js';
 import { Device, Status, ControlMode, } from './enums.js';
 
 function ReactiveConnectable(args = {}) {
+    // config
     const deviceType = args.deviceType ?? Device.generic;
     const filter = args.filter ?? webBle.filters.generic();
     const identifier = `ble:${deviceType}`;
 
     const connectable = Connectable({
-        filter: webBle.filters.controllable(),
+        filter,
         onData,
         onConnecting,
         onConnected,
         onDisconnect,
+        onConnectFail,
     });
+    // end config
+
+    // state
+    let mode = ControlMode.erg;
+
+    let abortController;
+    let signal;
+    // end state
 
     function getIdentifier() {
         return identifier;
     }
 
     function onConnecting() {
-        xf.dispatch(`ble:${deviceType}:connecting`);
+        xf.dispatch(`${getIdentifier()}:connecting`);
     }
 
     function onConnected() {
@@ -35,6 +45,10 @@ function ReactiveConnectable(args = {}) {
         xf.dispatch(`${getIdentifier()}:name`, '--');
     }
 
+    function onConnectFail() {
+        xf.dispatch(`${getIdentifier()}:disconnected`);
+    }
+
     function onData(data) {
         if('power' in data && models.sources.isSource('power', identifier)) {
             xf.dispatch(`power`, data.power);
@@ -45,8 +59,7 @@ function ReactiveConnectable(args = {}) {
         }
 
         if('speed' in data && models.sources.isSource('speed', identifier)) {
-            // xf.dispatch(`speed`, models.speed.kmhToMps(data.speed));
-            xf.dispatch(`speed`, data.speed);
+            xf.dispatch(`speed`, models.speed.kmhToMps(data.speed));
         }
 
         if('heartRate' in data && models.sources.isSource('heartRate', identifier)) {
@@ -70,7 +83,6 @@ function ReactiveConnectable(args = {}) {
         }
     }
 
-    let mode = ControlMode.erg;
     function onMode(x) {
         mode = x;
     }
@@ -79,27 +91,27 @@ function ReactiveConnectable(args = {}) {
     }
 
     function onPowerTarget(powerTarget) {
-        connectable.trainer.control.setPowerTarget({power: powerTarget});
+        if(!connectable.isConnected()) return;
+        connectable.services.trainer.setPowerTarget({power: powerTarget});
     }
 
     function onResistanceTarget(resistanceTarget) {
-        connectable.trainer.control.setResistanceTarget({
+        if(!connectable.isConnected()) return;
+        connectable.services.trainer.setResistanceTarget({
             resistance: resistanceTarget,
         });
     }
 
     function onSlopeTarget(slopeTarget) {
-        connectable.trainer.control.setSimulation({grade: slopeTarget});
+        if(!connectable.isConnected()) return;
+        connectable.services.trainer.setSimulation({grade: slopeTarget});
     }
-
-    let abortController;
-    let signal;
 
     function start() {
         abortController = new AbortController();
         signal = { signal: abortController.signal };
 
-        xf.sub(`ui:${getIdentifier()}:switch`,onSwitch, signal);
+        xf.sub(`ui:${getIdentifier()}:switch`, onSwitch, signal);
 
         if(equals(deviceType, Device.controllable)) {
             xf.sub('db:mode',             onMode, signal);
