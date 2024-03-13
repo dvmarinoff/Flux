ec0aafdddfc10c86c7d168c2bdd01cae715df0a6
dvmarinoff <dvmarinoff@gmail.com>
2024-03-13 20:26:06 +0200

    add EventType 'enum'

---
+++ src/activity/enums.js
@@ -0,0 +1,32 @@
+const TimerStatus = {
+    stopped: 'stopped',
+    started: 'started',
+    paused: 'paused',
+    resumed: 'resumed',
+};
+
+const TimerAction = {
+    start: 'start',
+    stop: 'stop',
+    pause: 'pause',
+};
+
+const RideStatus = {
+    stopped: 'stopped',
+    started: 'started',
+    paused: 'paused',
+    stopping: 'stopping',
+};
+
+const EventType = {
+    start: 'start',
+    stop: 'stop',
+};
+
+export {
+    TimerStatus,
+    TimerAction,
+    RideStatus,
+    EventType,
+};
+
